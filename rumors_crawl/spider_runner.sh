289ba08efc8c4f614f4f3c317440c5e9938e424b
Jose Ayerdis <necronet@Joses-MacBook-Pro.local>
2018-03-09 12:42:15 -0500

    Add spider runner for queue to cron task

---
+++ rumors_crawl/spider_runner.sh
@@ -0,0 +1,4 @@
+scrapy crawl politifact -t json -o data/politifact/$(date "+%Y-%m-%d-%H-%M-%S").json
+scrapy crawl buzzfeed -t json -o data/buzzfeed/$(date "+%Y-%m-%d-%H-%M-%S").json
+scrapy crawl snopes -t json -o data/snopes/$(date "+%Y-%m-%d-%H-%M-%S").json
+#scrapy crawl politifact -t json -o data/politifact/$(date "+%Y-%m-%d-%H-%M-%S").json
