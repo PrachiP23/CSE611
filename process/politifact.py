df29bccfeb4bf288c9fdee78422e8120a527c3b0
Jose Ayerdis <necronet@Joses-MacBook-Pro.local>
2018-03-22 16:37:35 -0400

    Quickly fix the regexp for politifact as it was crashing for st and nd dates

--- process/politifact.py
+++ process/politifact.py
@@ -1,6 +1,6 @@
 from process.process import Process
 from datetime import datetime
-
+import re
 
 class PolitifactProcess(Process):
 
@@ -8,6 +8,17 @@ class PolitifactProcess(Process):
         document = super().process()
         document["innerTitle"] = ''.join(document["innerTitle"]).strip()
         document['content'] = document['content'].strip()
-        document['date'] = datetime.strptime(document['date'].replace('.',''), 'on %A, %B %dth, %Y at %H:%M %p').strftime('%d-%m-%Y')
+
+        #note: one coule apply a more complex regexp but for
+        # now we just want to clean this
+        preffix = "th"
+
+        if re.search(r"[\d\d]st",document['date']):
+            preffix = "st"
+        elif re.search(r"[\d\d]nd",document['date']):
+            preffix = "nd"
+
+        pattern = 'on %A, %B %d{}, %Y at %H:%M %p'.format(preffix)
+        document['date'] = datetime.strptime(document['date'].replace('.',''), pattern).strftime('%d-%m-%Y')
 
         return document
