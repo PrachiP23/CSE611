327953c1e77e2f26ed1789fccbb0e63175bebbf8
Jose Ayerdis <necronet@Joses-MBP.fios-router.home>
2018-04-09 02:39:20 -0400

    Starting gathering data from twitter using tdidf topics analyisis and cursor tweepy api and storing it in a specific folder

--- twitter/base.py
+++ twitter/base.py
@@ -28,7 +28,38 @@ if __name__ == '__main__':
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+    if (not api):
+        print ("Can't Authenticate")
+        sys.exit(-1)
+
+
+    files = ["./SNOPES.JSON","POLITIFACT.JSON","BUZZFEED.JSON","URBAN_LEGENDS.JSON"]
+
+    counter = 0
+    for file in files:
+        try:
+            documents = json.load(open(file))
+            for document in documents:
+                counter = counter + 1
+                query = ' '.join(document['terms'])
+                print(query)
+                searched_tweets = [status._json for status in tweepy.Cursor(api.search, q=query).items(1000)]
+                file = 'output/{}.json'.format(''.join(document['terms']))
+
+                if searched_tweets:
+                    with open(file, 'w', encoding='utf8') as fp:
+                        json.dump(searched_tweets, fp)
+        except FileNotFoundError:
+            print("File does not exists")
+
+
+    #new_tweets = api.search(q=' '.join(terms), count=100, lang="en")
+    #print(new_tweets)
+
+    #stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['carrot','chlorin','babi','cut'])
+    #stream.filter(track=['carrot','chlorin','babi','cut'])
