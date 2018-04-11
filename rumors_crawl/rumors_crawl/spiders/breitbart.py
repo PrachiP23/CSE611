88eabaf0a47827339df53c557d3c47ac68b47754
PrachiP23 <ppatil3@buffalo.edu>
2018-04-11 04:21:13 -0400

    Breitbart Scrapy
    Breitbart Scrapy

---
+++ rumors_crawl/rumors_crawl/spiders/breitbart.py
@@ -0,0 +1,28 @@
+import scrapy
+#from scrapy.exceptions import CloseSpider
+
+class Breitbart(scrapy.Spider):
+
+    name="breitbart"
+    start_urls=['http://www.breitbart.com/']
+
+    def parse(self, response):
+
+        articles = response.xpath('//article/h2/a/@href | //div[@id="BBTrendNow"]/ul/li/a/@href')
+
+        for href in articles.extract():
+            if href is not None:
+                yield response.follow(href, callback=self.parse_article)
+
+
+    def parse_article(self,response):
+        article_info = response.xpath('//div[@id="MainW"]/article')
+        titleClass = article_info.xpath('./header/h1')
+        date_time = article_info.xpath('./header/p/span[2]')
+        content = article_info.xpath('./div')
+        yield{
+                'title': titleClass.extract_first(),
+                'referred_url':response.request.url,
+                'date': date_time.extract_first(),
+                'content':''.join(content.extract()),
+                }
