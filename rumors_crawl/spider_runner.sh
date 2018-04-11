13ff201018879d7f98a9fce6f03f3f683018d8a9
PrachiP23 <ppatil3@buffalo.edu>
2018-04-11 06:32:52 -0400

    updated spider_runner
    updated spider_runner

--- rumors_crawl/spider_runner.sh
+++ rumors_crawl/spider_runner.sh
@@ -2,3 +2,5 @@ scrapy crawl politifact -t json -o data/politifact/$(date "+%Y-%m-%d-%H-%M-%S").
 scrapy crawl buzzfeed -t json -o data/buzzfeed/$(date "+%Y-%m-%d-%H-%M-%S").json
 scrapy crawl snopes -t json -o data/snopes/$(date "+%Y-%m-%d-%H-%M-%S").json
 scrapy crawl urbanlegends -t json -o data/urbanlegends/$(date "+%Y-%m-%d-%H-%M-%S").json
+scrapy crawl dailyWire -t json -o data/dailyWire/$(date "+%Y-%m-%d-%H-%M-%S").json
+scrapy crawl breitbart -t json -o data/breitbart/$(date "+%Y-%m-%d-%H-%M-%S").json
